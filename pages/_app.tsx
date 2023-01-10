a8c726f1d9ebcf13d8e8558fcc7a2304cffe6a89
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-10 19:23:53 -0300

    adding useEffect and useState to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import type { AppContext, AppProps } from 'next/app'
 import { CssBaseline, Theme, ThemeProvider } from '@mui/material'
 import { darkTheme, lightTheme, customTheme } from '../themes';
+import Cookies from 'js-cookie';
+import { useState, useEffect } from 'react';
 
 
 // extendemos a AppProps y le agregamos la propiedad de theme
@@ -9,14 +11,23 @@ interface Props extends AppProps {
   theme: string
 }
 
-export default function App({ Component, pageProps, theme }: Props) {
+export default function App({ Component, pageProps, theme = 'dark' }: Props) {
 
   // console.log({ theme })
-  const currentTheme: Theme = theme === 'light'
-    ? lightTheme
-    : theme === 'dark'
-      ? darkTheme
-      : customTheme
+  const [currentTheme, setCurrentTheme] = useState(lightTheme)
+
+
+  useEffect(() => {
+    const cookieTheme = Cookies.get('theme') || 'light'
+
+    const selectedTheme: Theme = cookieTheme === 'light'
+      ? lightTheme
+      : cookieTheme === 'dark'
+        ? darkTheme
+        : customTheme
+
+    setCurrentTheme(selectedTheme)
+  }, [])
 
 
   return (
@@ -28,11 +39,11 @@ export default function App({ Component, pageProps, theme }: Props) {
   )
 }
 
-App.getInitialProps = async (appContext: AppContext) => {
-  const validTheme = ['light', 'dark', 'custom']
-  const cookies = appContext.ctx.req ? (appContext.ctx.req as any).cookies : { theme: 'light' }
-  console.log('GetInitialProps:', cookies)
-  return {
-    theme: validTheme.includes(cookies.theme) ? cookies.theme : 'dark',
-  }
-}
+// App.getInitialProps = async (appContext: AppContext) => {
+//   const validTheme = ['light', 'dark', 'custom']
+//   const cookies = appContext.ctx.req ? (appContext.ctx.req as any).cookies : { theme: 'light' }
+//   console.log('GetInitialProps:', cookies)
+//   return {
+//     theme: validTheme.includes(cookies.theme) ? cookies.theme : 'dark',
+//   }
+// }
