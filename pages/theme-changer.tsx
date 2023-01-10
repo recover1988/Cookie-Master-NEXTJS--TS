1d886410716f0d74b8630a89d9773b24eb3eac62
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-10 18:29:57 -0300

    use cookie in back, SSR,front

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,10 +1,13 @@
-import React, { ChangeEvent, useEffect, useState } from 'react'
+import React, { ChangeEvent, FC, useEffect, useState } from 'react'
 import { Layout } from '../components/layouts'
-import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
-
+import { Button, Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
+import { GetServerSideProps } from 'next'
 import Cookies from 'js-cookie'
+import axios from 'axios'
 
-const ThemeChangerPage = () => {
+
+
+const ThemeChangerPage: FC = (props) => {
     const [currentTheme, setCurrentTheme] = useState('light')
 
     const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -15,8 +18,14 @@ const ThemeChangerPage = () => {
         //Cookie
         Cookies.set('theme', selectedTheme)
     }
+
+    const onClick = async () => {
+        const {data} = await axios.get('/api/hello')
+    }
+
     useEffect(() => {
         console.log('LocalStorage:', localStorage.getItem('theme'))
+        console.log('Cookies:', Cookies.get('theme'))
         // se puede guardar hasta 5mb de informacion en el LocalStorage
         // axios.post('/api/hello', localStorage.getItem('theme'))
     }, [])
@@ -36,11 +45,32 @@ const ThemeChangerPage = () => {
                             <FormControlLabel value='custom' control={<Radio />} label='Custom' />
                         </RadioGroup>
                     </FormControl>
+                    <Button
+                        onClick={onClick}
+                    >
+                        Solicitud
+                    </Button>
                 </CardContent>
             </Card>
 
         </Layout>
     )
+}
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+
+    // const {cookies} = req.cookies // next ya lo serializa y devuelve el objeto cookie con su key=value
+    const { theme = 'light', name = 'No name' } = req.cookies
+
+    return {
+        props: {
+            theme,
+            name
+        }
+    }
 }
 
 export default ThemeChangerPage
