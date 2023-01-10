b4ed8935f5bfab2e6672947467d193ff1855c248
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-10 16:03:02 -0300

    adding options radio in theme-changer.tsx

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { Layout } from '../components/layouts'
+import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
 
 const ThemeChangerPage = () => {
+    const [currentTheme, setCurrentTheme] = useState('light')
+
+    const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const selectedTheme = event.target.value
+        setCurrentTheme(selectedTheme)
+    }
     return (
         <Layout>
+            <Card>
+                <CardContent>
+                    <FormControl>
+                        <FormLabel>Tema</FormLabel>
+                        <RadioGroup
+                            value={currentTheme}
+                            onChange={onThemeChange}
+                        >
+                            <FormControlLabel value='light' control={<Radio />} label='Light' />
+                            <FormControlLabel value='dark' control={<Radio />} label='Dark' />
+                            <FormControlLabel value='custom' control={<Radio />} label='Custom' />
+                        </RadioGroup>
+                    </FormControl>
+                </CardContent>
+            </Card>
 
-            <h1>ThemeChangerPage</h1>
         </Layout>
     )
 }
